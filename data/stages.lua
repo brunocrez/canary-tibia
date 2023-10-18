ad17ab89e3e25d705042107b65f2c89c8a7d4f4a
Bruno Rezende <brunoc.rez@gmail.com>
2023-10-18 07:50:55 -0500

    feat: balancing exp and skills stages

--- data/stages.lua
+++ data/stages.lua
@@ -4,85 +4,90 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 8,
-		multiplier = 7,
+		maxlevel = 1000,
+		multiplier = 250,
 	},
 	{
-		minlevel = 9,
-		maxlevel = 20,
-		multiplier = 6,
+		minlevel = 1001,
+		maxlevel = 1500,
+		multiplier = 200,
 	},
 	{
-		minlevel = 21,
-		maxlevel = 50,
+		minlevel = 1501,
+		maxlevel = 2000,
+		multiplier = 150,
+	},
+	{
+		minlevel = 2001,
+		maxlevel = 2500,
+		multiplier = 100,
+	},
+	{
+		minlevel = 2501,
+		maxlevel = 3000,
+		multiplier = 75,
+	},
+	{
+		minlevel = 3001,
+		maxlevel = 3500,
+		multiplier = 50,
+	},
+	{
+		minlevel = 3501,
+		maxlevel = 4000,
+		multiplier = 25,
+	},
+	{
+		minlevel = 4001,
+		maxlevel = 4500,
+		multiplier = 12.5,
+	},
+	{
+		minlevel = 4501,
 		multiplier = 5,
-	},
-	{
-		minlevel = 51,
-		maxlevel = 100,
-		multiplier = 4,
-	},
-	{
-		minlevel = 101,
-		multiplier = 2,
 	},
 }
 
 skillsStages = {
 	{
 		minlevel = 10,
-		maxlevel = 60,
+		maxlevel = 39,
+		multiplier = 20,
+	},
+	{
+		minlevel = 40,
+		maxlevel = 69,
 		multiplier = 15,
 	},
 	{
-		minlevel = 61,
-		maxlevel = 80,
+		minlevel = 70,
+		maxlevel = 99,
 		multiplier = 10,
 	},
 	{
-		minlevel = 81,
-		maxlevel = 110,
-		multiplier = 6,
-	},
-	{
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 4,
-	},
-	{
-		minlevel = 126,
-		multiplier = 2,
+		minlevel = 100,
+		multiplier = 5,
 	},
 }
 
 magicLevelStages = {
 	{
-		minlevel = 0,
-		maxlevel = 60,
+		minlevel = 10,
+		maxlevel = 39,
+		multiplier = 20,
+	},
+	{
+		minlevel = 40,
+		maxlevel = 69,
+		multiplier = 15,
+	},
+	{
+		minlevel = 70,
+		maxlevel = 99,
 		multiplier = 10,
 	},
 	{
-		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 7,
-	},
-	{
-		minlevel = 81,
-		maxlevel = 100,
+		minlevel = 100,
 		multiplier = 5,
-	},
-	{
-		minlevel = 101,
-		maxlevel = 110,
-		multiplier = 4,
-	},
-	{
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 3,
-	},
-	{
-		minlevel = 126,
-		multiplier = 2,
 	},
 }
